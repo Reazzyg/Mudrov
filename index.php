6c5f833c36d0d210a5cf60cce40522571d443f2b
Reazzyg <mudrsemen@ya.ru>
2024-08-29 18:24:53 +0300

    !

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once('header.php')
 
     <h2 class="subtitle hero-subtitle">
       <span class="hero-subtitle__text">
-        Поможем вашему бизнесу в интернете с помощью комплексного подхода в asdasdasd
+        Поможем вашему бизнесу в интернете с помощью комплексного подхода в маркетинге
       </span>
     </h2>
     <!-- /.subtitle hero-subtitle -->
